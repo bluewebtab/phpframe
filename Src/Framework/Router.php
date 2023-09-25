67ea5412db633d2dd7751bb02c6a863bcaf58ea8
Ronny Gutierrez <ronny.icne@gmail.com>
2023-09-25 14:49:17 -0700

    Normalized paths

--- Src/Framework/Router.php
+++ Src/Framework/Router.php
@@ -9,11 +9,21 @@ class Router
 {
     private array $routes = [];
 
-    public function add(string $method, string $path){
+    public function add(string $method, string $path)
+    {
+        $path = $this->normalizePath($path);
         $this->routes[] = [
             'path' => $path,
             'method' => strtoupper($method)
         ];
 
+    }
+
+    private function normalizePath(string $path): string
+    {
+        $path = trim($path, '/');
+        $path = "/{$path}";
+
+        return $path;
     }
 }
