f1c8d7797f1a6e7cd3f8bce9bdd805e3acdd0413
Ronny Gutierrez <ronny.icne@gmail.com>
2023-09-25 16:54:17 -0700

    Added dispatch function for routes

--- Src/Framework/Router.php
+++ Src/Framework/Router.php
@@ -9,12 +9,13 @@ class Router
 {
     private array $routes = [];
 
-    public function add(string $method, string $path)
+    public function add(string $method, string $path, array $controller)
     {
         $path = $this->normalizePath($path);
         $this->routes[] = [
             'path' => $path,
-            'method' => strtoupper($method)
+            'method' => strtoupper($method),
+            'controller' => $controller
         ];
 
     }
@@ -27,5 +28,13 @@ class Router
         $path = preg_replace('#[/]{2,}#', '/', $path);
 
         return $path;
+    }
+
+    public function dispatch(string $path, string $method)
+    {
+        $path = $this->normalizePath($path);
+        $method = strtoupper($method);
+
+        echo $path . $method;
     }
 }
