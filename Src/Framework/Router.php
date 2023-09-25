91e76516ed8c8018af39af03f6ea671086d35886
Ronny Gutierrez <ronny.icne@gmail.com>
2023-09-25 15:31:46 -0700

    Added reg expressions

--- Src/Framework/Router.php
+++ Src/Framework/Router.php
@@ -22,7 +22,9 @@ class Router
     private function normalizePath(string $path): string
     {
         $path = trim($path, '/');
-        $path = "/{$path}";
+        $path = "/{$path}/";
+
+        $path = preg_replace('#[/]{2,}#', '/', $path);
 
         return $path;
     }
