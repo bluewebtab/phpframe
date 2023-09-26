54c34fa332035dda2e93e49fe56b994c87197d3f
Ronny Gutierrez <ronny.icne@gmail.com>
2023-09-25 22:53:51 -0700

    Base path setup

--- Src/App/Controllers/HomeController.php
+++ Src/App/Controllers/HomeController.php
@@ -5,11 +5,22 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+use App\Config\Paths;
+
 
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine(Paths::VIEW);
+    }
     public function home()
     {
+
+        dd($this->view);
         echo 'home page';
     }
 }
